2f09008ddd3a2a1ebf63e7f11f98f6ae6db4873c
Nancy Jo <enciie@gmail.com>
2018-08-02 00:13:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,6 +18,8 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  # Code your final two routes here:
-
+  get "/multiply/:num1/:num2"
+    @sum = num1 * num2
+    "@sum"
+  end
 end
